0cd73889dbd77775cc3c7ccda8ddf57ecab3e157
Slade Getz <asgetz@mines.edu>
2022-08-29 15:30:48 -0600

    finished start script

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cpu_count="$(( $(sysctl hw.physicalcpu_max | sed 's/[^0-9]* \([0-9*]\)/\1/') / 2))"
+if [[ $(( cpu_count%2 )) -eq 1 ]]; then
+  cpu_count=$((cpu_count - 1))
+fi
+
+mem="$(( $(sysctl hw.memsize | sed 's/[^0-9]* \([0-9*]\)/\1/') / 1024**2 / 2))"
+
+qemu-system-x86_64 -serial stdio \
+ -drive file=SEED-Ubuntu20.04.vdi,format=vdi -m $mem \
+ -vga vmware -accel tcg,thread=multi \
+ -smp $cpu_count \
+ -device qemu-xhci \
+ -M q35 \
+ -cpu qemu64 
