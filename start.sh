ed7d917ae28c0b24ae6a807580b6c94d71a09cef
Slade Getz <asgetz@mines.edu>
2022-08-29 17:38:51 -0600

    updated start.sh for easier custom configuration

--- start.sh
+++ start.sh
@@ -5,12 +5,18 @@ if [[ $( basename "$(pwd)" ) != "SEEDLabs-m1" ]]; then
   exit 0
 fi
 
-cpu_count="$(( $(sysctl hw.physicalcpu_max | sed 's/[^0-9]* \([0-9*]\)/\1/') / 2))"
+cpu_count="$(( ($(sysctl hw.physicalcpu_max | sed 's/[^0-9]* \([0-9*]\)/\1/') - 2) / 2))"
 if [[ $(( cpu_count%2 )) -eq 1 ]]; then
   cpu_count=$((cpu_count - 1))
 fi
 
 mem="$(( $(sysctl hw.memsize | sed 's/[^0-9]* \([0-9*]\)/\1/') / 1024**2 / 2))"
+
+
+# custom configs
+## if using custom, I suggest commenting out the lines that calculate cpu count and memory (above)
+# cpu_count=2
+# mem=2048
 
 qemu-system-x86_64 -serial stdio \
  -drive file=SEED-Ubuntu20.04.vdi,format=vdi -m $mem \
